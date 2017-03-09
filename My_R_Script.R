6c3d6e40f7de03529da95b2bf119f5af8d467a55
Tanguy <tlevent02@gmail.com>
2017-03-09 09:35:21 +0100

    push my work

--- My_R_Script.R
+++ My_R_Script.R
@@ -1,23 +1,23 @@
 library(dplyr)
-library(ggplot2)
-library(RColorBrewer)
 library(PerformanceAnalytics)
+
+
+# Processing Data
 
 data("mtcars")
 sum(is.na(mtcars))
 
+head(mtcars)
+str(mtcars)
+
 # Exploratory analysis
 chart.Correlation(mtcars, histogram=TRUE, pch=19)
+dev.off()
 
 mdata <- mtcars %>% select(mpg,cyl,disp,hp,drat,wt,vs,am)
-
 mdata$cyl <- factor(mdata$cyl)
 mdata$vs <- factor(mdata$vs)
 mdata$am = factor(mdata$am, labels = c("Automatic","Manual"))
-
-# Summary the selection
-head(mdata)
-str(mdata)
 
 # Statistical inference
 
@@ -25,10 +25,17 @@ t.test(mpg~am, paired = FALSE, var.equal = FALSE, mdata)
 
 # Regression linear
 
-mpg.lm.global <- lm(mpg ~ ., mdata)
-mpg.lm.selection <- step(mpg.lm.global, direction = "both")
 mpg.lm.am <- lm(mpg ~ am, mdata)
+round(sigma(mpg.lm.am),2)
+round(summary(mpg.lm.am)$adj.r.squared,3)
 
-summary(mpg.lm.selection)
+mpg.lm.global <- lm(mpg ~ ., mdata)
+round(sigma(mpg.lm.global),2)
+round(summary(mpg.lm.global)$adj.r.squared,3)
+
+mpg.lm.selection <- step(mpg.lm.global, direction = "both", trace=FALSE)
+round(sigma(mpg.lm.selection),2)
+round(summary(mpg.lm.selection)$adj.r.squared,3)
+
 anova(mpg.lm.selection,mpg.lm.am)
 
